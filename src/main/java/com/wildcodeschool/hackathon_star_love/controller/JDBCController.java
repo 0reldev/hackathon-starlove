ea66a207e701b2305951f6c663fca124465c18ba
swann <chevreauswann@outlook.fr>
2020-04-29 16:39:26 +0200

    création PeopleRepository + route jdbc

--- src/main/java/com/wildcodeschool/hackathon_star_love/controller/JDBCController.java
+++ src/main/java/com/wildcodeschool/hackathon_star_love/controller/JDBCController.java
@@ -1,8 +1,24 @@
 package com.wildcodeschool.hackathon_star_love.controller;
 
+import com.wildcodeschool.hackathon_star_love.entity.People;
+import com.wildcodeschool.hackathon_star_love.repository.PeopleRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@Controller
 public class JDBCController {
+
+    private PeopleRepository repository = new PeopleRepository();
+
+    @GetMapping("/peopleList")
+    @ResponseBody
+    public List<People>  getFindLoveGlobal() {
+
+        return repository.findLoveGlobal();
+    }
 
 
 }
