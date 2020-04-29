77e1cfbb5a03213389768206f3416d691889fc79
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-04-29 11:47:03 +0200

    Project initializing

---
+++ src/main/java/com/wildcodeschool/hackathon_star_love/HackathonStarLoveApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.hackathon_star_love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonStarLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonStarLoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/hackathon_star_love/HackathonStarLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.hackathon_star_love;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonStarLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
