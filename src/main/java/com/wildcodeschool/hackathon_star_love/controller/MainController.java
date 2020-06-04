cc1bf44ef7c2c4a39664c55a104320cc5e330eee
bastienwcs <bastien@wldcodeschool.fr>
2020-06-04 16:50:37 +0200

    add deploy conf

--- src/main/java/com/wildcodeschool/hackathon_star_love/controller/MainController.java
+++ src/main/java/com/wildcodeschool/hackathon_star_love/controller/MainController.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.hackathon_star_love.controller;
 
 import com.wildcodeschool.hackathon_star_love.entity.People;
 import com.wildcodeschool.hackathon_star_love.repository.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    private PeopleRepository repository = new PeopleRepository();
+    @Autowired
+    private PeopleRepository repository;
 
     @GetMapping("/index")
     public String showIndex() {
@@ -21,8 +23,6 @@ public class MainController {
 
     @GetMapping("/result")
     public String showResult(Model out) {
-
-        PeopleRepository repository = new PeopleRepository();
 
         People people = repository.findLoveGlobal();
         out.addAttribute("people", people);
@@ -33,8 +33,6 @@ public class MainController {
     @GetMapping("/result-bis")
     public String showPeople(Model out, @RequestParam String planetName) {
 
-        PeopleRepository repository = new PeopleRepository();
-
         People people = repository.findLovePlanet(planetName);
         out.addAttribute("people", people);
 
@@ -44,8 +42,6 @@ public class MainController {
     @GetMapping("/result-3")
     public String showPeopleChoice(Model out, @RequestParam (required = false) String planetName,
                                    @RequestParam (required = false) String gender, @RequestParam (required = false) String eyeColor, @RequestParam (required = false) String hairColor) {
-
-        PeopleRepository repository = new PeopleRepository();
 
         People people = repository.findLoveChoice(planetName, gender, eyeColor, hairColor);
         out.addAttribute("people", people);
